ab91ce9917ed6e9926127fc2c26784ef0aa3775e
Ikenna Okpala <ikennaokpala@users.noreply.github.com>
2015-07-16 14:54:55 +0100

    Cosmetic change
    Camelcase fix

--- includes/assets/javascript/gfb_subscribe.js
+++ includes/assets/javascript/gfb_subscribe.js
@@ -54,8 +54,8 @@ GFBSubscribe.prototype.isValidEmail = function(entry) {
    return (entry.indexOf(".") > 2) && (entry.indexOf("@") > 0);
 }
 
-GFBSubscribe.prototype.scrollManager = function(isNotLoggedin) {
-  if(isNotLoggedin) {
+GFBSubscribe.prototype.scrollManager = function(isNotLoggedIn) {
+  if(isNotLoggedIn) {
     var docBody = document.body,
     docElement = document.documentElement,
     height
