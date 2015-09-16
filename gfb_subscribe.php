3f331be03b5cfee236fe5467aa334e2294d75f54
Ikenna Okpala <me@ikennaokpala.com>
2015-09-16 04:09:21 +0100

    Bump version

--- gfb_subscribe.php
+++ gfb_subscribe.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package MailChimp Subscribe For Food & Cook Theme by Global Food Book
- * @version 1.30
+ * @version 1.40
  */
 /*
 Plugin Name: MailChimp Subscribe For Food & Cook Theme
 Plugin URI: http://wordpress.org/extend/plugins/mailchimp-subscribe-for-food-cook-theme/
 Description: This plugin is an extract from <a href='http://globalfoodbook.com' target='_blank'>globalfoodbook.com</a>. This plugin will work only on websites that have the <a href='http://themeforest.net/item/food-cook-multipurpose-food-recipe-wp-theme/4915630'>food-cook</a> template installed  and have setup mailchimp connect url, See food-cooke docs and <a href='http://kb.mailchimp.com/lists/managing-subscribers/find-your-list-id' target='_blank'>this</a> for more details. This plugin is built to help other <a href='http://themeforest.net/item/food-cook-multipurpose-food-recipe-wp-theme/4915630'>food-cook</a> site owners (from the support group) who require this utility. It is implemented to allow easy setup and customization of a website's newsletter subscription widget and modal popup. It is best used with food and cook recipe theme made with woo themes.
 Author: Ikenna N. Okpala
-Version: 1.30
+Version: 1.40
 Author URI: http://ikennaokpala.com/
 */
 // File Security Check
